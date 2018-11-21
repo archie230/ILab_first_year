9acfe53bacb8d7ad41e7f7d217f8a4c422391958
archie230 <43552079+archie230@users.noreply.github.com>
2018-11-21 23:13:56 +0300

    Update onegin-sort.c

--- FileSorter/onegin-sort.c
+++ FileSorter/onegin-sort.c
@@ -1,6 +1,8 @@
 //______________________________________________________________________________________________________________________
-
-//filling text buff
+/** \brief This func reading text from file
+ * @param N_symbols 
+ * @param buff_sym 
+ */
 
 void read_txt(int* N_symbols, char** buff_sym)
 {
@@ -34,9 +36,12 @@ void read_txt(int* N_symbols, char** buff_sym)
     fclose(data);
 }
 //______________________________________________________________________________________________________________________
-/* This func counting and returning number of symbols
-   in your file
-*/
+/**  \brief This func counting number of symbols
+ * This func counting and returning number of symbols in
+ * your txt file
+ * @param data 
+ * @return 
+ */
 
 int counting_nsymbols(FILE* data)
 {
@@ -48,9 +53,10 @@ int counting_nsymbols(FILE* data)
     return N_symbols;
 }
 //______________________________________________________________________________________________________________________
-/* This func counting number of lines
-   in your txtbuffer
-*/
+/** \brief This func counting number of lines
+ * @param N_lines 
+ * @param buff_sym 
+ */
 
 void count_nlines(int* N_lines, char** buff_sym)
 {
@@ -64,9 +70,15 @@ void count_nlines(int* N_lines, char** buff_sym)
 
 }
 //______________________________________________________________________________________________________________________
-/* This func creating and filling array
-   of structs and original_txt
-*/
+/** \brief This func creating arrays
+ * This func creating and filling array
+ * of structs and original_txt
+ * @param txt 
+ * @param original_txt 
+ * @param buff_sym 
+ * @param N_lines 
+ */
+
 
 void create_arrays(string** txt, char*** original_txt, char** buff_sym, int* N_lines)
 {
@@ -89,8 +101,10 @@ void create_arrays(string** txt, char*** original_txt, char** buff_sym, int* N_l
     }
 }
 //______________________________________________________________________________________________________________________
-/* This func coding russian alphabet
-*/
+/** \brief This func coding symbols
+ * @param letter 
+ * @return 
+ */
 
 int Ru_char_encoding(char* letter)
 {
@@ -101,7 +115,7 @@ int Ru_char_encoding(char* letter)
     if(!letter)
     {
         printf("NULL pointer in encoding");
-        free(alph); 
+        free(alph);
     }
 
     if (counter == 0)
@@ -139,7 +153,13 @@ int Ru_char_encoding(char* letter)
 
 }
 //______________________________________________________________________________________________________________________
-/* This func compare two strings by alphabet*/
+/** \brief This func compare two strings by alphabet
+ * 
+ * @param str1 
+ * @param str2 
+ * @param char_encoding 
+ * @return 
+ */
 
 int my_strcmp(char* str1, char* str2, int (*char_encoding) (char* letter))
 {
@@ -177,7 +197,15 @@ int my_strcmp(char* str1, char* str2, int (*char_encoding) (char* letter))
         return -1;
 }
 //______________________________________________________________________________________________________________________
-/* This func compare rwo strings by ryphm */
+/** \brief This func compare rwo strings by ryphm 
+ * 
+ * @param str1 
+ * @param str2 
+ * @param len1 
+ * @param len2 
+ * @param char_encoding 
+ * @return 
+ */
 
 int my_strcmp_ryphm(char* str1, char* str2, int len1, int len2, int (*char_encoding) (char* letter))
 {
@@ -206,6 +234,11 @@ int comp_ryphm(const void* x1, const void* x2)
     return my_strcmp_ryphm((*(const string*) x1).s_ptr, (*(const string*) x2).s_ptr, (*(const string*) x1).length, (*(const string*) x2).length, Ru_char_encoding);
 }
 //______________________________________________________________________________________________________________________
+/** \brief This func sort and write array of strings
+ * @param txt 
+ * @param N_lines 
+ * @param comp 
+ */
 void sort_andwrite(string** txt, int N_lines, int (*comp) (const void* x1, const void* x2))
 {
     FILE* out = fopen(output, "a");
@@ -232,6 +265,11 @@ void sort_andwrite(string** txt, int N_lines, int (*comp) (const void* x1, const
     fclose(out);
 }
 //______________________________________________________________________________________________________________________
+/**
+ * \brief This func write origin txt
+ * @param original_txt 
+ * @param N_lines 
+ */
 void write_originaltxt(char*** original_txt, int N_lines)
 {
     FILE* out = fopen(output, "a");
